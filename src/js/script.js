b41f9a05f7bbc62957d6ea61399b890a45c3deeb
Yevhenii <evgengorobets5@gmail.com>
2022-08-21 00:46:58 +0400

    added tabs and it`s content

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,36 @@
-window.addEventListener('DOMContentLoaded', () => {
-    $(document).ready(function(){
-        $('.carousel__inner').slick({
-            // fade: true,
-            // cssEase: 'linear',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 1000,
-            // autoplay: true,
-            adaptiveHeight: true,
-            prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.svg"></button>',
-            nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.svg"></button>',
-            responsive: [
-                {
-                    breakpoint: 992,
-                    settings: {
-                        dots: true,
-                        arrows: false
-                        }
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // fade: true,
+        // cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        // autoplay: true,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prevArrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/nextArrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
                     }
-                ]
+                }
+            ]
+        });
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+              .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+              .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
           });
-      });
+
+          $('.catalog-item__link').each( (i) => {
+            $(this).on('click', (e) => {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('.catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+          });
 });
+
