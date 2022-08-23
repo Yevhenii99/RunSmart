1ae4e4268407a04f80346e6fbce0bb9e25b2a21e
Yevhenii <evgengorobets5@gmail.com>
2022-08-23 21:24:22 +0400

    added modal window

--- src/js/script.js
+++ src/js/script.js
@@ -25,12 +25,77 @@ $(document).ready(function(){
               .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
           });
 
-          $('.catalog-item__link').each( (i) => {
-            $(this).on('click', (e) => {
-                e.preventDefault();
-                $('.catalog-item__content').eq(i).toggleClass('.catalog-item__content_active');
-                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
-          });
+          
+          function toggleSlide(item) {
+            $(item).each(function(i) {
+                $(this).on('click', function(e) {
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                });
+              });
+          }
+          toggleSlide('.catalog-item__back');
+          toggleSlide('.catalog-item__link');
+
+          //Modal
+
+
+
+          const btn = document.querySelectorAll('[data-modal=consultation]'),
+                modal = document.querySelector('.modal'),
+                cover = document.querySelector('.cover'),
+                close = document.querySelector('.modal-btn-close'),
+                btnMini = document.querySelectorAll('[data-modal="order"]'),
+                orderModal = document.querySelector('[data-modal="form"]');
+
+
+
+
+                btn.forEach((item) => {
+                    item.addEventListener('click', () => {
+                        openModal();
+                    });
+                });
+
+                btnMini.forEach((item) => {
+                    item.addEventListener('click', () => {
+                        openOrderModal();
+                        // console.log('click');
+                    });
+                });
+
+                function openModal() {
+                    cover.classList.add('show');
+                    modal.classList.add('show');
+                }
+
+                function openOrderModal() {
+                    orderModal.classList.add('show');
+                    cover.classList.add('show');
+                }
+
+
+                cover.addEventListener('click', (event) => {
+                    if (event.target === cover && cover.classList.contains('show')) {
+                        closeModal();
+                    }
+                });
+                close.addEventListener('click', () => {
+                    closeModal();
+                });
+                document.addEventListener('keydown', (e) => {
+                    if (e.code === "Escape" && cover.classList.contains('show')) { 
+                        closeModal();
+                    }
+                });
+
+          function closeModal() {
+                    cover.classList.remove('show');
+                    modal.classList.remove('show');
+            }
+
+
+        
 });
 
