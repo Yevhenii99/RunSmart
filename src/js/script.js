29b79d9eb335f0e6d003062ba1e4e378e3e9ab91
Yevhenii <evgengorobets5@gmail.com>
2022-08-26 13:07:19 +0400

    added interactive modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -25,75 +25,97 @@ $(document).ready(function(){
               .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
           });
 
-          
-          function toggleSlide(item) {
-            $(item).each(function(i) {
-                $(this).on('click', function(e) {
-                    e.preventDefault();
-                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-                });
-              });
-          }
-          toggleSlide('.catalog-item__back');
-          toggleSlide('.catalog-item__link');
+    
+    function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+        });
+    }
+    toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
 
-          //Modal
+    //Modal
 
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
 
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
 
-          const btn = document.querySelectorAll('[data-modal=consultation]'),
-                modal = document.querySelector('.modal'),
-                cover = document.querySelector('.cover'),
-                close = document.querySelector('.modal-btn-close'),
-                btnMini = document.querySelectorAll('[data-modal="order"]'),
-                orderModal = document.querySelector('[data-modal="form"]');
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
 
-
-
-
-                btn.forEach((item) => {
-                    item.addEventListener('click', () => {
-                        openModal();
-                    });
-                });
-
-                btnMini.forEach((item) => {
-                    item.addEventListener('click', () => {
-                        openOrderModal();
-                        // console.log('click');
-                    });
-                });
-
-                function openModal() {
-                    cover.classList.add('show');
-                    modal.classList.add('show');
-                }
-
-                function openOrderModal() {
-                    orderModal.classList.add('show');
-                    cover.classList.add('show');
-                }
+    // const btn = document.querySelectorAll('[data-modal=consultation]'),
+    //     modal = document.querySelector('[data-modal="first-form"]'),
+    //     cover = document.querySelector('.cover'),
+    //     close = document.querySelector('.modal-btn-close'),
+    //     btnMini = document.querySelectorAll('[data-modal="order"]'),
+    //     orderModal = document.querySelector('[data-modal="second-form"]');
 
 
-                cover.addEventListener('click', (event) => {
-                    if (event.target === cover && cover.classList.contains('show')) {
-                        closeModal();
-                    }
-                });
-                close.addEventListener('click', () => {
-                    closeModal();
-                });
-                document.addEventListener('keydown', (e) => {
-                    if (e.code === "Escape" && cover.classList.contains('show')) { 
-                        closeModal();
-                    }
-                });
 
-          function closeModal() {
-                    cover.classList.remove('show');
-                    modal.classList.remove('show');
-            }
+
+
+    //     btn.forEach((item) => {
+    //         item.addEventListener('click', () => {
+    //             openModal();
+    //         });
+    //     });
+
+    //     function openModal() {
+    //         cover.classList.add('show');
+    //         modal.classList.add('show');
+    //     }
+
+    //     cover.addEventListener('click', (event) => {
+    //         if (event.target === cover && cover.classList.contains('show')) {
+    //             closeModal();
+    //             closeOrderModal();
+    //         }
+    //     });
+    //     close.addEventListener('click', () => {
+    //         closeModal();
+    //         closeOrderModal();
+    //     });
+    //     document.addEventListener('keydown', (e) => {
+    //         if (e.code === "Escape" && cover.classList.contains('show')) { 
+    //             closeModal();
+    //             closeOrderModal();
+    //         }
+    //     });
+
+
+    //     function closeOrderModal() {
+    //         cover.classList.remove('show');
+    //         orderModal.classList.remove('show');
+    //     }
+
+    //     function closeModal() {
+    //         cover.classList.remove('show');
+    //         modal.classList.remove('show');
+    //     }
+
+
+    //     //btnMini
+
+    //     btnMini.forEach((item) => {
+    //         item.addEventListener('click', () => {
+    //             cover.classList.add('show');
+    //             orderModal.classList.add('show');
+    //         });
+    //     });
+
+
 
 
         
